dcbbf866e67de7c76c9afd10166476feec539b06
Azza BOUTOUR <azza.boutour@gmail.com>
2017-11-24 11:57:32 +0100

    function multiply of two matrix

--- matrixmultiply.cpp
+++ matrixmultiply.cpp
@@ -11,11 +11,23 @@ uint8* MatrixMultiply::apply(uint8 *I, const unsigned int sizeY)
 {
     uint8 *Y = new uint8[sizeKx * sizeY];
 
+
     for (unsigned int iX = 0; iX < sizeKx; ++iX)
         {
-        for (unsigned int iY = 0; iY < sizeKy; ++iY)
+        for (unsigned int iY = 0; iY < sizeY; ++iY)
             {
+	   
+	      Y[iX][iY] = 0;
+	      for (int z = 0; z < sizeKy; z++)
+	      Y[iX][iY] += I[iX][z] * K[z][iY];
+	    }
 
-            }
-        }
+	}
 }
+		
+		  
+		    
+		    
+		    
+		 
+   
